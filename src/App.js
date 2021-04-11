e2ff187b99fe040acd4b9957c638250534ee3a2a
Todor Konjevic <todor.konjevic@gmail.com>
2021-04-11 16:28:27 +0200

    Add Header component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello world!
+    <div className="App container mx-auto px-10">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header py-10 text-poppins text-2xl leading-9">
+      <span className="font-bold">Github</span> Jobs
+    </div>
+  );
+};
+
+export default Header;
