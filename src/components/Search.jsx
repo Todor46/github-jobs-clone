143d6f99fd79b43c2de842c465b3c55bf9cfd7bd
Todor Konjevic <todor.konjevic@gmail.com>
2021-04-13 13:02:38 +0200

    Fix border radius on search bar.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,16 +6,16 @@ const Search = () => {
       className="search-container rounded-lg w-full py-10.5 bg-cover"
       style={{ backgroundImage: 'url("backgroundImg.png")' }}
     >
-      <div className="search-bar w-2/3 mx-auto p-1 bg-white rounded-lg flex items-center">
-        <span class="material-icons text-lg text-gray-400 ml-4">
+      <div className="search-bar w-2/3 mx-auto p-1 bg-white rounded-4 flex items-center">
+        <span class="material-icons text-lg text-gray-light ml-4">
           work_outline
         </span>
         <input
           placeholder="Title, companies, expertise or benefits"
           type="text"
-          className="w-full text-xs border-none focus:ring-0"
+          className="w-full text-xs border-none focus:ring-0 placeholder-gray-light"
         />
-        <button className="text-white font-medium focus:outline-none leading-4 bg-brand hover:bg-brand-light rounded-md py-3.5 px-12">
+        <button className="text-white font-medium focus:outline-none leading-4 bg-brand hover:bg-brand-light rounded-4 py-3.5 px-12">
           Search
         </button>
       </div>
