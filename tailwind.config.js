151bbf40bd0b5392b5ae2bf71f7851b924f07cb8
Todor Konjevic <todor.konjevic@gmail.com>
2021-04-13 13:01:59 +0200

    Update tailwind config file with new colors and checked sibling plugin.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+const plugin = require("tailwindcss/plugin");
+
+const checkedSiblingPlugin = plugin(function ({ addVariant, e }) {
+  addVariant("checked-sibling", ({ container }) => {
+    container.walkRules((rule) => {
+      rule.selector = `:checked + .checked-sibling\\:${rule.selector.slice(1)}`;
+    });
+  });
+});
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
@@ -8,15 +18,27 @@ module.exports = {
           light: '#4092f1',
           DEFAULT: '#1E86FF',
           dark: '#1a75df'
+        },
+        gray: {
+          dark: '#334680',
+          light: '#B9BDCF',
+          lighter: '#f6f7fb'
         }
       },
       spacing: {
+        4.5: '1.15rem',
         10.5: '2.65rem'
+      },
+      borderRadius: {
+        4: '4px'
       }
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["checked-sibling"],
+      borderColor: ["checked-sibling"]
+    },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), checkedSiblingPlugin],
 }
