2974256d9ea251109005b6e595d873a6eaa2542d
Todor Konjevic <todor.konjevic@gmail.com>
2021-04-13 17:56:50 +0200

    Replace mock data with props in JobCard.

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,30 +1,48 @@
 import React from "react";
+import formatJobDate from "../utils/formatJobDate";
 
-const JobCard = () => {
+const JobCard = ({ job }) => {
+  const {
+    id,
+    type,
+    company,
+    company_url,
+    location,
+    title,
+    company_logo,
+    created_at,
+  } = job;
+
   return (
-    <div className="bg-white w-full rounded-4 p-3 flex space-x-3">
-      <div className="w-22 h-22">
-        <img src="devchallenges.png" alt="job" className="w-full" />
+    <div className="bg-white w-full rounded-4 p-3 flex space-x-4 shadow-sm">
+      <div className="w-22 h-22 flex items-center">
+        <img src={company_logo} alt={company} className="w-full" />
       </div>
       <div className="flex flex-col flex-grow">
-        <div className="text-xs font-bold mb-2">Kasisto</div>
-        <div className="text-lg leading-5 mb-4">Front-End Engineer</div>
+        <div className="text-xs font-bold mb-2">
+          <a href={company_url} target="blank">
+            {company}
+          </a>
+        </div>
+        <div className="text-lg leading-5 mb-3">
+          <a href={`/${id}`}>{title}</a>
+        </div>
         <div className="flex justify-between">
           <div className="border border-gray-dark rounded-4 py-1 px-2 text-xs font-bold w-min whitespace-nowrap flex-none">
-            Full time
+            {type}
           </div>
           <div className="flex text-gray-light space-x-7 text-xs">
             <div className="flex items-center">
               <span className="material-icons text-lg mr-1 leading-3">
                 public
               </span>
-              New York
+              {location}
             </div>
             <div className="flex items-center">
               <span className="material-icons text-lg mr-1  leading-3">
                 schedule
               </span>
-              5 days ago
+              {formatJobDate(created_at)}
             </div>
           </div>
         </div>
