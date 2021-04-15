7e9261994834ae65fdec5d6fd13a0f9f8eff871b
Todor Konjevic <todor.konjevic@gmail.com>
2021-04-15 16:40:51 +0200

    Add footer.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import "./App.css";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           </Route>
         </Switch>
       </Router>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full text-center text-gray-light mt-16 text-sm text-montserrat font-medium">
+      created by{" "}
+      <a
+        className="font-bold underline"
+        target="blank"
+        href="https://github.com/Todor46"
+      >
+        Todor Konjevic
+      </a>{" "}
+      - devChallenges.io
+    </div>
+  );
+};
+
+export default Footer;
